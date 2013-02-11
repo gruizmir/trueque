630350769e0339d6f865e6238724c5e59215bc66
Astom <astom@ubuntu.ubuntu-domain>
2013-02-11 17:25:16 -0300

    Usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,12 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
 from django.db import models
 
 class Country(models.Model):
@@ -18,8 +27,8 @@ class Usuario(models.Model):
     usuario_password = models.CharField(max_length=75, db_column='Usuario_password') # Field name made lowercase.
     usuario_name = models.CharField(max_length=60, db_column='Usuario_name') # Field name made lowercase.
     usuario_lastname = models.CharField(max_length=60, db_column='Usuario_lastname') # Field name made lowercase.
-    usuario_bullletins = models.IntegerField(db_column='Usuario_bullletins') # Field name made lowercase.
-    usuario_email_1 = models.CharField(max_length=90, db_column='Usuario_email_1') # Field name made lowercase.
+    usuario_bulletins = models.BooleanField(db_column='Usuario_bulletins') # Field name made lowercase.
+    usuario_email_1 = models.CharField(max_length=90, unique=True, db_column='Usuario_email_1') # Field name made lowercase.
     usuario_email_2 = models.CharField(max_length=90, db_column='Usuario_email_2', blank=True) # Field name made lowercase.
     usuario_phone_1 = models.CharField(max_length=30, db_column='Usuario_phone_1', blank=True) # Field name made lowercase.
     usuario_phone_2 = models.CharField(max_length=30, db_column='Usuario_phone_2', blank=True) # Field name made lowercase.
@@ -28,31 +37,46 @@ class Usuario(models.Model):
     usuario_tw_token = models.CharField(max_length=480, db_column='Usuario_TW_Token', blank=True) # Field name made lowercase.
     usuario_register_date = models.DateTimeField(db_column='Usuario_register_date') # Field name made lowercase.
     usuario_city = models.ForeignKey(City, null=True, db_column='Usuario_city', blank=True) # Field name made lowercase.
-    usuario_level = models.IntegerField(db_column='Usuario_level') # Field name made lowercase.
-    usuario_rating = models.IntegerField(db_column='Usuario_rating') # Field name made lowercase.
-    usuario_ranking_qty = models.IntegerField(null=True, db_column='Usuario_ranking_qty', blank=True) # Field name made lowercase.
-    usuario_quds = models.IntegerField(db_column='Usuario_quds') # Field name made lowercase.
-    usuario_follower_qty = models.IntegerField(db_column='Usuario_follower_qty') # Field name made lowercase.
-    usuario_followe_qty = models.IntegerField(db_column='Usuario_followe_qty') # Field name made lowercase.
-    usuario_barter_qty = models.IntegerField(db_column='Usuario_barter_qty') # Field name made lowercase.
-    usuario_remaining_invitations = models.IntegerField(db_column='Usuario_remaining_invitations') # Field name made lowercase.
-    usuario_art = models.IntegerField(db_column='Usuario_art') # Field name made lowercase.
-    usuario_music = models.IntegerField(db_column='Usuario_music') # Field name made lowercase.
-    usuario_tech = models.IntegerField(db_column='Usuario_tech') # Field name made lowercase.
-    usuario_cars = models.IntegerField(db_column='Usuario_cars') # Field name made lowercase.
-    usuario_travels = models.IntegerField(db_column='Usuario_travels') # Field name made lowercase.
-    usuario_clothes = models.IntegerField(db_column='Usuario_clothes') # Field name made lowercase.
-    usuario_cine = models.IntegerField(db_column='Usuario_cine') # Field name made lowercase.
-    use_sports = models.IntegerField(db_column='Use_sports') # Field name made lowercase.
-    usuario_eco = models.IntegerField(db_column='Usuario_eco') # Field name made lowercase.
-    usuario_culture = models.IntegerField(db_column='Usuario_culture') # Field name made lowercase.
-    usuario_spectacles = models.IntegerField(db_column='Usuario_spectacles') # Field name made lowercase.
-    usuario_love = models.IntegerField(db_column='Usuario_love') # Field name made lowercase.
-    usuario_food = models.IntegerField(db_column='Usuario_food') # Field name made lowercase.
-    usuario_vacations = models.IntegerField(db_column='Usuario_vacations') # Field name made lowercase.
-    usuario_services = models.IntegerField(db_column='Usuario_services') # Field name made lowercase.
+    usuario_level = models.IntegerField(db_column='Usuario_level', default='0') # Field name made lowercase.
+    usuario_rating = models.IntegerField(db_column='Usuario_rating', default='0') # Field name made lowercase.
+    usuario_ranking_qty = models.IntegerField(null=True, db_column='Usuario_ranking_qty', blank=True, default='0') # Field name made lowercase.
+    usuario_quds = models.IntegerField(db_column='Usuario_quds', default='0') # Field name made lowercase.
+    usuario_follower_qty = models.IntegerField(db_column='Usuario_follower_qty', default='0') # Field name made lowercase.
+    usuario_followed_qty = models.IntegerField(db_column='Usuario_followed_qty', default='0') # Field name made lowercase.
+    usuario_barter_qty = models.IntegerField(db_column='Usuario_barter_qty', default='0') # Field name made lowercase.
+    usuario_remaining_invitations = models.IntegerField(db_column='Usuario_remaining_invitations', default='15') # Field name made lowercase.
+    usuario_art = models.BooleanField(db_column='Usuario_art') # Field name made lowercase.
+    usuario_music = models.BooleanField(db_column='Usuario_music') # Field name made lowercase.
+    usuario_tech = models.BooleanField(db_column='Usuario_tech') # Field name made lowercase.
+    usuario_cars = models.BooleanField(db_column='Usuario_cars') # Field name made lowercase.
+    usuario_travels = models.BooleanField(db_column='Usuario_travels') # Field name made lowercase.
+    usuario_clothes = models.BooleanField(db_column='Usuario_clothes') # Field name made lowercase.
+    usuario_cine = models.BooleanField(db_column='Usuario_cine') # Field name made lowercase.
+    usuario_sports = models.BooleanField(db_column='Usuario_sports') # Field name made lowercase.
+    usuario_eco = models.BooleanField(db_column='Usuario_eco') # Field name made lowercase.
+    usuario_culture = models.BooleanField(db_column='Usuario_culture') # Field name made lowercase.
+    usuario_spectacles = models.BooleanField(db_column='Usuario_spectacles') # Field name made lowercase.
+    usuario_love = models.BooleanField(db_column='Usuario_love') # Field name made lowercase.
+    usuario_food = models.BooleanField(db_column='Usuario_food') # Field name made lowercase.
+    usuario_vacations = models.BooleanField(db_column='Usuario_vacations') # Field name made lowercase.
+    usuario_services = models.BooleanField(db_column='Usuario_services') # Field name made lowercase.
     class Meta:
         db_table = u'Usuario'
+
+class Rating(models.Model):
+    id_rating = models.IntegerField(primary_key=True, db_column='ID_rating') # Field name made lowercase.
+    id_rater = models.ForeignKey(Usuario, null=True, db_column='ID_rater', related_name='rating_rater', blank=True) # Field name made lowercase.
+    id_rated = models.ForeignKey(Usuario, db_column='ID_rated', related_name='rating_rated') # Field name made lowercase.
+    rating_level = models.IntegerField(db_column='Rating_level') # Field name made lowercase.
+    rating_datetime = models.DateTimeField(db_column='Rating_datetime') # Field name made lowercase.
+    class Meta:
+        db_table = u'Rating'
+
+class Followers(models.Model):
+    id_follower = models.ForeignKey(Usuario, primary_key=True, db_column='ID_follower', related_name='followers_follower') # Field name made lowercase.
+    id_followed = models.ForeignKey(Usuario, db_column='ID_followed', related_name='followers_followed') # Field name made lowercase.
+    class Meta:
+        db_table = u'Followers'
 
 class Warningreason(models.Model):
     id_reason = models.IntegerField(primary_key=True, db_column='ID_reason') # Field name made lowercase.
@@ -60,27 +84,12 @@ class Warningreason(models.Model):
     class Meta:
         db_table = u'WarningReason'
 
-class Warnings(models.Model):
+class Warning(models.Model):
     id_warning = models.IntegerField(primary_key=True, db_column='ID_warning') # Field name made lowercase.
-    id_sender = models.ForeignKey(Usuario, db_column='ID_sender') # Field name made lowercase.
-    id_receiver = models.ForeignKey(Usuario, db_column='ID_receiver', related_name="+") # Field name made lowercase.
+    id_sender = models.ForeignKey(Usuario, db_column='ID_sender', related_name='warning_sender') # Field name made lowercase.
+    id_receiver = models.ForeignKey(Usuario, db_column='ID_receiver', related_name='warning_receiver') # Field name made lowercase.
     warning_reason = models.ForeignKey(Warningreason, null=True, db_column='Warning_reason', blank=True) # Field name made lowercase.
     warning_message = models.TextField(db_column='Warning_message') # Field name made lowercase.
     warning_datetime = models.IntegerField(db_column='Warning_datetime') # Field name made lowercase.
     class Meta:
         db_table = u'Warning'
-
-class Rating(models.Model):
-    id_rating = models.IntegerField(primary_key=True, db_column='ID_rating') # Field name made lowercase.
-    id_rater = models.ForeignKey(Usuario, null=True, db_column='ID_rater', blank=True, related_name="+") # Field name made lowercase.
-    id_rated = models.ForeignKey(Usuario, db_column='ID_rated') # Field name made lowercase.
-    rating_level = models.IntegerField(db_column='Rating_level') # Field name made lowercase.
-    rating_datetime = models.DateTimeField(db_column='Rating_datetime') # Field name made lowercase.
-    class Meta:
-        db_table = u'Rating'
-
-class Followers(models.Model):
-    id_follower = models.ForeignKey(Usuario, primary_key=True, db_column='ID_follower') # Field name made lowercase.
-    id_followed = models.ForeignKey(Usuario, db_column='ID_followed', related_name="+") # Field name made lowercase.
-    class Meta:
-        db_table = u'Followers'
