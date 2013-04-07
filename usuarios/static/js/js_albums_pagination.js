20c3522b9b277654ed9c133b6701e4b3b7cfce67
Astom <astom@ubuntu.ubuntu-domain>
2013-04-07 18:47:09 -0400

    modificacion vista perfil, se añade scrollbar y pagination

--- usuarios/static/js/js_albums_pagination.js
+++ usuarios/static/js/js_albums_pagination.js
@@ -7,11 +7,13 @@ function albumsPagination(){
 		$.get(url + this.id, function(data) {
 			result = data.albums_data;
 			$('#result')[0].innerHTML = result;
+			$(".album_polaroids_container").mCustomScrollbar();
 			albumsPagination();
 		});
 	});
 }
 
 $(document).ready(function() {
+	$(".album_polaroids_container").mCustomScrollbar();
 	albumsPagination();
 });
