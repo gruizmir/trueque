b847590de1f71a0642467a0ed52c2243e3477462
Gabriel Ruiz M <gabriel.ruiz.miranda@gmail.com>
2013-06-29 18:36:51 -0400

    a

--- trueque/settings.py
+++ trueque/settings.py
@@ -243,5 +243,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.user.create_user',
     'social_auth.backends.pipeline.social.associate_user',
     'social_auth.backends.pipeline.social.load_extra_data',
-    'social_auth.backends.pipeline.user.update_user_details'
+    'social_auth.backends.pipeline.user.update_user_details',
+    'social_auth.backends.pipeline.misc.save_status_to_session',
 )
